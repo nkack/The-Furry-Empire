040955fac9165be35c2ddf170073caddeeb1447f
nkack <nikolaikack@icloud.com>
2023-12-20 18:51:38 +0100

    Started addRole command

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord import Member
 from discord.ext.commands import has_permissions, MissingPermissions
+from discord.utils import get
+
 
 class Moderation(commands.Cog):
 
@@ -13,6 +15,13 @@ class Moderation(commands.Cog):
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
         await ctx.send(f'User {member} has been kicked')
+
+    @commands.command(pass_context = True)
+    @commands.has_permissions(manage_roles = True)
+    async def addRole(self, ctx, user : discord.Member, *, role : discord.role):
+
+        if role in user.roles:
+            await ctx.send("This user already has the role")
 
 
     @kick.error
